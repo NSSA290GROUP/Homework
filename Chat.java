1872b82353a26b009e7a33ed2a2c86069088d93e
dap2154 <dap2154@rit.edu>
2016-04-10 17:25:31 -0400

    Added all the ActionListeners functions except Join the chat and send handler

--- Chat.java
+++ Chat.java
@@ -1,88 +1,153 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-
+import javax.swing.border.*;
 /** 
- * Start Chat
- *
+ * @author  Abigail Spring, David Mauriello, David Player
+ * The purpose of this program is to connect between client and server using TCP and UDP Protocols. 
  */
-public class Chat extends JFrame implements ActionListener
+public class Chat extends JFrame
 {
-
+ //Global attribues 
+   private JMenuBar menu; 
+   private  JMenu fileMenu; 
    private JTextArea jtaChat;
-   private JTextArea jtaMsg;
+   private JTextArea jtaMsgReceived, typingMessage;
    private JButton jbSend;
-
-	// Default Constructor
+   private JPanel jpChatBox, jpPanel; 
+   private JMenuItem jmiNewChat, jmiJoinChat,jmiQuit, jmiHelp, jmiClose; 
+   private JLabel jlStatus; 
+ 
+   public static void main(String[] args){
+      new Chat();  
+   }
+	// Chat Constructor
    public Chat()
    {
-      JMenuBar menu = new JMenuBar();
+      menu = new JMenuBar();
       setJMenuBar(menu);
       
-      JMenu fileMenu = new JMenu("File");
+      fileMenu = new JMenu("File");
       menu.add(fileMenu);
       
-      JMenuItem jmiNewChat = new JMenuItem("New Chat Window");
-      JMenuItem jmiJoinChat = new JMenuItem("Join Chat");
-      JMenuItem jmiQuit = new JMenuItem("Quit");
-      JMenuItem jmiHelp = new JMenuItem("Help");
-      JMenuItem jmiClose = new JMenuItem("Close");
+      jmiNewChat = new JMenuItem("New Chat Window");
+      //Opening a new Chat window  
+      jmiNewChat.addActionListener(
+            new ActionListener(){
+               public void actionPerformed(ActionEvent ae){
+                  new Chat().setVisible(true); 
+               }
+            });
+   
+      jmiJoinChat = new JMenuItem("Join Chat");
       
+      //Joining Chat: Will have to add networking method here
+      jmiJoinChat.addActionListener(
+         new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+             //Adding networking here
+            }
+         }
+         );
+      jmiHelp = new JMenuItem("Help");
+      //A help instruction for users how to use a Chat program 
+      jmiHelp.addActionListener(
+            new ActionListener(){
+               public void actionPerformed(ActionEvent ae){
+               
+                  String help = "New Chat Window: users to opening up a new window\n" + 
+                     "Join chat: join in a current chat session\n" + 
+                     "Close the chat: Ask an user if User want to leave chat session \n" + 
+                     "Quit: Quit the chat session";
+                  JOptionPane.showMessageDialog(null, help, "Chat program" ,  JOptionPane.PLAIN_MESSAGE );   
+               }
+            });
+   
+      
+      jmiClose = new JMenuItem("Close");
+      //Prompting a question if user is really want to leave a current chat session
+      jmiClose.addActionListener(
+         new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+               int response = JOptionPane.showConfirmDialog(null,"Do you want to leave chat room?", "Confirm", JOptionPane.YES_NO_OPTION); 
+               
+               if(response == JOptionPane.NO_OPTION){
+               
+               }
+               else if(response == JOptionPane.YES_OPTION){
+                  System.exit(0); 
+               }
+            }
+         }
+         );
+       
+      jmiQuit = new JMenuItem("Quit");
+      //Close the chat session immediately 
+      jmiQuit.addActionListener(
+            new ActionListener(){
+               public void actionPerformed(ActionEvent ae){
+                  System.exit(0); 
+               }
+            });
+   
+      //Add menu items to main menu 
       fileMenu.add(jmiNewChat);
       fileMenu.add(jmiJoinChat);
       fileMenu.add(jmiQuit);
-      fileMenu.add(jmiHelp);
       fileMenu.add(jmiClose);
+      fileMenu.add(jmiHelp);
       
-   	// jpInfo to hold text fields and labels
-      JPanel jpChat = new JPanel();
-      jpChat.setLayout(new BorderLayout());
-      jpChat.add(jtaChat = new JTextArea(25,25), BorderLayout.CENTER);
-      
-      JPanel jpMsg = new JPanel();
-      jpMsg.setLayout(new FlowLayout());
-      jpMsg.add(jtaMsg = new JTextArea(50,50));
-      jpMsg.add(jbSend = new JButton("Send"));
+      //Sending your comments to Upper row that receive your messages
+      jtaMsgReceived = new JTextArea(5,30);
+      jtaMsgReceived.setBorder(new EtchedBorder());
+      jtaMsgReceived.setLineWrap( true ); 
+      jtaMsgReceived.setWrapStyleWord( true );  
    
-     	// Add panels to the frame
-      add(jpChat, BorderLayout.CENTER);
-      add(jpMsg, BorderLayout.SOUTH);
+    //Chat Box Layout is 2 row and one column 
+      jpChatBox = new JPanel(); 
+      jpChatBox.setLayout(new GridLayout(2,1)); 
+     
+      jpChatBox.add(jtaMsgReceived);
+    
+    //ChatBox is positioned at Center  in JFrame 
+      add(jtaMsgReceived, BorderLayout.CENTER);
+   
+      typingMessage = new JTextArea(5,30); 
       
-   	// Register listeners
-      jbSend.addActionListener(this);
+      jpPanel = new JPanel(new FlowLayout(FlowLayout.CENTER,10,15)); 
+   
+      jpPanel.add(typingMessage); 
+      jbSend = new JButton("SEND"); 
+      jpPanel.add(jbSend);
+      
+      add(jpPanel, BorderLayout.SOUTH); 
+   	
+       //Adding ChatHandler ActionListener Object to the Send Button   
+      //jbSend.addActionListener();
+      
+      //GUI Visible
+      setTitle("Chat");
+      pack();
+      setLocationRelativeTo(null); 
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setVisible(true);
    }
 
 
-	// Handle ActionEvent from buttons
-   public void actionPerformed(ActionEvent e)
-   {
-      if(e.getActionCommand().equals("Add")) {
-         //add message to jtaChat
-      }
-   
-   } // end actionPerformed()
-   
-   public void run() {
-      //try/catch maybe? 
-      //method to like set the connection and then JOptionPane for the prompt which is below commented out
-      //method will need 3 arguments, the third one returns an integer
-      
-      //Object[] options = {"TCP/IP",
-      //              "UDP"};
-      /*
-         JOptionPane.showInputDialog("Enter an IP address:"), 
-                       JOptionPane.showInputDialog("Enter your name:"), 
-                       JOptionPane.showOptionDialog(frame, "Select a protocol to use:", "Protocol Choice", JOptionPane.YES_NO_OPTION, null, null, options, options[0]));
-      */
-   }
+  
+   // public void run() {
+   //    //try/catch maybe? 
+   //    //method to like set the connection and then JOptionPane for the prompt which is below commented out
+   //    //method will need 3 arguments, the third one returns an integer
+   //    
+   //    //Object[] options = {"TCP/IP",
+   //    //              "UDP"};
+   //    /*
+   //       JOptionPane.showInputDialog("Enter an IP address:"), 
+   //                     JOptionPane.showInputDialog("Enter your name:"), 
+   //                     JOptionPane.showOptionDialog(frame, "Select a protocol to use:", "Protocol Choice", JOptionPane.YES_NO_OPTION, null, null, options, options[0]));
+   //    */
+   // }
 
-   public static void main(String[] args){
-      Chat startFrame = new Chat();
-      
-      startFrame.setTitle("Chat");
-      startFrame.pack();
-      startFrame.setLocation(20,20);
-      startFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      startFrame.setVisible(true);
-   }
-} // end Client class
+  
+} // end Chat class
