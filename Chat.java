8211a1ac281a6d69e9b446ecee41540f7c74e567
dap2154 <dap2154@rit.edu>
2016-04-12 18:07:14 -0400

    Added files via upload

--- Chat.java
+++ Chat.java
@@ -2,6 +2,10 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.*;
+import java.io.*; 
+import java.net.*;
+import java.util.*; 
+
 /** 
  * @author  Abigail Spring, David Mauriello, David Player
  * The purpose of this program is to connect between client and server using TCP and UDP Protocols. 
@@ -12,14 +16,22 @@ public class Chat extends JFrame
    private JMenuBar menu; 
    private  JMenu fileMenu; 
    private JTextArea jtaChat;
-   private JTextArea jtaMsgReceived, typingMessage;
+   private JTextArea jtaMsgReceived, jtfSend;
    private JButton jbSend;
    private JPanel jpChatBox, jpPanel; 
    private JMenuItem jmiNewChat, jmiJoinChat,jmiQuit, jmiHelp, jmiClose; 
    private JLabel jlStatus; 
+   private String host, userName;
+   private boolean pickTCP, pickUDP; 
+   private Socket cs; 
+   private BufferedReader input; 
+   private PrintWriter output; 
+   
  
    public static void main(String[] args){
-      new Chat();  
+    
+      
+      new ClientPrompt(); 
    }
 	// Chat Constructor
    public Chat()
@@ -41,13 +53,15 @@ public class Chat extends JFrame
    
       jmiJoinChat = new JMenuItem("Join Chat");
       
-      //Joining Chat: Will have to add networking method here
+      //Joining Chat: Will have to add networking here
       jmiJoinChat.addActionListener(
-         new ActionListener(){
-            public void actionPerformed(ActionEvent ae){
-             //Adding networking here
+            new ActionListener(){
+               public void actionPerformed(ActionEvent ae){
+             
+                new ClientPrompt(); 
+                  //Adding networking here
+               }
             }
-         }
          );
       jmiHelp = new JMenuItem("Help");
       //A help instruction for users how to use a Chat program 
@@ -67,18 +81,18 @@ public class Chat extends JFrame
       jmiClose = new JMenuItem("Close");
       //Prompting a question if user is really want to leave a current chat session
       jmiClose.addActionListener(
-         new ActionListener(){
-            public void actionPerformed(ActionEvent ae){
-               int response = JOptionPane.showConfirmDialog(null,"Do you want to leave chat room?", "Confirm", JOptionPane.YES_NO_OPTION); 
+            new ActionListener(){
+               public void actionPerformed(ActionEvent ae){
+                  int response = JOptionPane.showConfirmDialog(null,"Do you want to leave chat room?", "Confirm", JOptionPane.YES_NO_OPTION); 
                
-               if(response == JOptionPane.NO_OPTION){
-               
-               }
-               else if(response == JOptionPane.YES_OPTION){
-                  System.exit(0); 
+                  if(response == JOptionPane.NO_OPTION){
+                  
+                  }
+                  else if(response == JOptionPane.YES_OPTION){
+                     System.exit(0); 
+                  }
                }
             }
-         }
          );
        
       jmiQuit = new JMenuItem("Quit");
@@ -112,18 +126,27 @@ public class Chat extends JFrame
     //ChatBox is positioned at Center  in JFrame 
       add(jtaMsgReceived, BorderLayout.CENTER);
    
-      typingMessage = new JTextArea(5,30); 
+      jtfSend = new JTextArea(5,30); 
       
       jpPanel = new JPanel(new FlowLayout(FlowLayout.CENTER,10,15)); 
    
-      jpPanel.add(typingMessage); 
+      jpPanel.add(jtfSend); 
       jbSend = new JButton("SEND"); 
       jpPanel.add(jbSend);
       
       add(jpPanel, BorderLayout.SOUTH); 
-   	
+      
+      //Chat Networking for connecting with server: FYI, this chatnetworking method is incomplete 
+      ChatNetworking(host, pickTCP, pickUDP);
+      
+      ChatHandler send = new ChatHandler(); 
        //Adding ChatHandler ActionListener Object to the Send Button   
-      //jbSend.addActionListener();
+      jbSend.addActionListener(send);
+      
+      //Sending and Receive the message from the server using Thread
+      Thread Reading = new Thread(new ReceivesMessage()); 
+      Reading.start(); 
+      
       
       //GUI Visible
       setTitle("Chat");
@@ -131,23 +154,141 @@ public class Chat extends JFrame
       setLocationRelativeTo(null); 
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       setVisible(true);
-   }
-
-
+   }// end Chat constructor
+   
+   //Chat Networking method 
+   public void ChatNetworking(String _host, boolean pickTCP, boolean pickUDP){
+   
+      host = _host; 
+     //I'm not sure if I understand Chat Networking method's requirement on our document because it isn't that clear as I thought
+      try{
+      
+         cs = new Socket("hostname", 16789); 
+         input = new BufferedReader(new InputStreamReader(cs.getInputStream())); 
+         output = new PrintWriter(new OutputStreamWriter(cs.getOutputStream())); 
+         //I'm not sure how to use TCP DataOutputSTream to create a socket. That means DataOutputStream replace PrintWriter?
+          DataOutputStream out = new DataOutputStream(cs.getOutputStream()); 
+         
+         System.out.println("getLocalHost: "+InetAddress.getLocalHost() );
+     
+      
+      }
+      catch (UnknownHostException uhe){
+         jtaMsgReceived.setText("Unable to conect the host"); 
+         return;
+      }
+      catch (IOException ioe){
+         jtaMsgReceived.setText("Communication lost"); 
+         return; 
+      }
+   
+   }//end ChatNetworking method  
+   
+   class ChatHandler implements ActionListener{
+      public void actionPerformed(ActionEvent ae){
+      
+         String message = jtfSend.getText();
+          
+         message = userName + ":" + message; 
+          
+         output.println(message); 
+         output.flush(); 
+         jtfSend.setText(" ");
+          
+      }//end actionPerformed
+   }//end ChatHandler class
+   
+   //Receives Message class thread
+   class ReceivesMessage implements Runnable{
+      public void run(){
+         String message; 
+         
+         try{
+            while((message = input.readLine()) != null){
+               System.out.println("Receives message: "+ message); 
+               jtaMsgReceived.append(message + "\n"); 
+            } 
+         }
+         catch(IOException ioe){
+            jtaMsgReceived.setText("Bad IO connection"); 
+         }
+      
+      }//end run
+   
+   }//end ReceviesMessage class 
+   
+} //end Chat class
   
-   // public void run() {
-   //    //try/catch maybe? 
-   //    //method to like set the connection and then JOptionPane for the prompt which is below commented out
-   //    //method will need 3 arguments, the third one returns an integer
-   //    
-   //    //Object[] options = {"TCP/IP",
-   //    //              "UDP"};
-   //    /*
-   //       JOptionPane.showInputDialog("Enter an IP address:"), 
-   //                     JOptionPane.showInputDialog("Enter your name:"), 
-   //                     JOptionPane.showOptionDialog(frame, "Select a protocol to use:", "Protocol Choice", JOptionPane.YES_NO_OPTION, null, null, options, options[0]));
-   //    */
-   // }
-
+  //Prompt Class 
+class ClientPrompt extends JFrame {
   
-} // end Chat class
+    //attributes
+   private JPanel jpNorth, jpCenter, jpSouth; 
+   private JLabel jlUserName, jlAddress, jlQuestion; 
+   private String getUserName, getAddress, host; 
+   private JTextField jtfUser, jtfAddress;
+   private int port; 
+   private JRadioButton jrTCP, jrUDP; 
+   private JButton jbSubmit; 
+
+    //ClientPrompt constructor  
+   public ClientPrompt(){
+        
+      jlUserName = new JLabel("UserName"); 
+      jlAddress = new JLabel("IP Address"); 
+      jlQuestion = new JLabel("Which protocol do you want to use?"); 
+        
+      jtfUser = new JTextField(20); 
+      jtfAddress = new JTextField(20); 
+        
+      jrTCP = new JRadioButton("TCP"); 
+      jrUDP = new JRadioButton("UDP"); 
+        
+      jbSubmit = new JButton("Submit"); 
+        
+        //A prompt GUI will be close after an user submitting an neccessary information to join a chat 
+      jbSubmit.addActionListener(
+            new ActionListener(){
+               public void actionPerformed(ActionEvent ae){
+                 
+                 //Chat object
+                  Chat users = new Chat(); 
+                
+                  setVisible(false); 
+                  dispose(); 
+               }//end actionPerformed
+            }//end ActionListener
+         );//end Submit Action Listener 
+        
+      jpNorth = new JPanel(); 
+      jpCenter = new JPanel(); 
+      jpSouth = new JPanel(); 
+        
+        
+      jpNorth.add(jlUserName); 
+      jpNorth.add(jtfUser); 
+        
+      jpCenter.add(jlAddress); 
+      jpCenter.add(jtfAddress); 
+        
+      jpSouth.add(jlQuestion); 
+      jpSouth.add(jrTCP); 
+      jpSouth.add(jrUDP); 
+        
+      jpSouth.add(jbSubmit); 
+        
+      add(jpNorth, BorderLayout.NORTH); 
+      add(jpCenter, BorderLayout.CENTER); 
+      add(jpSouth, BorderLayout.SOUTH); 
+        
+      setTitle("Client Prompt");
+      pack();
+      setLocationRelativeTo(null); 
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setVisible(true);     
+   } // end Client Prompt constructor 
+     
+  
+} // end Prompt Prompt class
+
+
